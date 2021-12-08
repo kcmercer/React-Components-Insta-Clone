4de90d0ad3377257ae9f6f047734b123ef5672df
Kenneth Mercer <kmercer5439@gmail.com>
2021-12-07 18:18:06 -0600

    got most sections working, but weird bugs

--- src/App.js
+++ src/App.js
@@ -30,19 +30,21 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
-    setPosts(posts.map(posts => {
+    const newPosts = posts.map(posts => {
       if(posts.id === postId){
         return {...posts, likes: posts.likes}
       } else {
         return posts;
       }
-    }))
+    });
+
+    setPosts(newPosts);
   };
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
-      <Posts likePost={likePost} />
+      <Posts posts={posts} likePost={likePost} />
       <SearchBar />
     </div>
   );
--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import Comments from '../Comments/Comments';
 import LikeSection from './LikeSection';
@@ -6,10 +7,6 @@ import PostHeader from './PostHeader';
 const Post = props => {
   // 🔥 Make sure the parent of Post is passing the right props!
   const { post, likePost } = props;
-
-  const like = () => {
-    return likePost(post.id);
-  }
 
   return (
     <div className='post-border'>
@@ -25,12 +22,9 @@ const Post = props => {
         />
       </div>
       {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection
-        likePost={like}
-        numberOfLikes={post.likes}
-      />
+      <LikeSection likePost={() => likePost(post.id)} numberOfLikes={post.likes} />
       {/* Comments also wants its props! */}
-      <Comments />
+      <Comments comments={post.comments}/>
     </div>
   );
 };
